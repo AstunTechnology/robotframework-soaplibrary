86df7c1eaf344f396a8fd226719602acab34ae52
Paul Porthouse <paulporthouse@astuntechnology.com>
2018-10-23 13:12:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
-"""Setup script for SoapLibrary for Robot Framework"""
+#!/usr/bin/env python
 
-from distutils.core import setup
+import os
+import sys
+from os.path import join, dirname
 
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+sys.path.append(join(dirname(__file__), 'src'))
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
 
-from SoapLibrary import __version__
+execfile(join(dirname(__file__), 'src', 'SoapLibrary', 'version.py'))
 
-def main():
-    setup(name         = 'SoapLibrary',
-          version      = __version__,
-          description  = 'iShare SOAP Testing Keywords for Robot Framework',
-          author       = 'Astun Technology',
-          author_email = '',
-          url          = '',
-          package_dir  = { '' : 'src'},
-          packages     = ['SoapLibrary'],
-          )
+DESCRIPTION = """
+SoapLibrary is a web service testing library for Robot Framework
+that leverages zeep to test SOAP-based services.
+""".strip()
 
+CLASSIFIERS  = """
+Development Status :: 5 - Development
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+""".strip().splitlines()
 
-if __name__ == "__main__":
-    main()
+import zeep
+
+setup(name         = 'robotframework-soaplibrary',
+      version      = VERSION,
+      description  = 'Robot Framework test library for SOAP-based services using zeep.',
+      long_description = DESCRIPTION,
+      author       = 'Astun Technology',
+      author_email = '<developers@astuntechnology.com>',
+      url          = 'https://github.com/AstunTechnology/robotframework-soaplibrary',
+      license      = 'Apache License 2.0',
+      keywords     = 'robotframework testing testautomation soap zeep web service',
+      platforms    = 'any',
+      classifiers  = CLASSIFIERS,
+      zip_safe     = True,
+      install_requires = [
+                            'zeep >= 3.1.0',
+                            'robotframework >= 3.0.0',
+                         ],
+      package_dir  = {'' : 'src'},
+      packages     = ['SoapLibrary']
+      )
