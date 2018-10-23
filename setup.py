7d088e470ce82d5ca21d8e68fb7f7ea47ee64381
Paul Porthouse <paulporthouse@astuntechnology.com>
2018-10-23 13:16:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ that leverages zeep to test SOAP-based services.
 """.strip()
 
 CLASSIFIERS  = """
-Development Status :: 5 - Development
+Development Status :: 3 - Alpha
 License :: OSI Approved :: Apache Software License
 Operating System :: OS Independent
 Programming Language :: Python
